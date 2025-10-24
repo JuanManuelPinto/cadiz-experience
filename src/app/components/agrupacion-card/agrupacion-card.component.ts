5eb50e497a4a3e660443b7004cfaa605291b5cad
JUAN MANUEL PINTO <jpingar2211@g.educaand.es>
2025-10-24 10:31:09 +0200

    mejora proyecto

--- src/app/components/agrupacion-card/agrupacion-card.component.ts
+++ src/app/components/agrupacion-card/agrupacion-card.component.ts
@@ -1,19 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Agrupacion } from 'src/app/models/agrupacion';
 import { CommonModule, DatePipe } from '@angular/common';
-import { IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonText, IonBadge } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonText, IonBadge, IonButton, IonImg, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { eye, eyeOutline } from 'ionicons/icons'; 
 
 @Component({
   selector: 'app-agrupacion-card',
   templateUrl: './agrupacion-card.component.html',
   styleUrls: ['./agrupacion-card.component.scss'],
   standalone: true,
-  imports: [CommonModule, DatePipe, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonText, IonBadge]
+  imports: [IonIcon, IonImg, IonButton, CommonModule, DatePipe, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonText, IonBadge]
 })
 export class AgrupacionCardComponent {
 
   @Input() agrupacion?: Agrupacion;
 
-  constructor() { }
+  constructor() {
+    addIcons({ eyeOutline });
+  }
 
 }
